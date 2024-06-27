66aaabb571ac790fa5f4a2309a6857a9101fd3e5
이연승 <leeys951006@gmail.com>
2024-06-27 20:52:03 +0900

    중간 저장

--- app.js
+++ app.js
@@ -7,9 +7,10 @@ const port = 3000;
 
 const server = http.createServer((req, res) => {
   if (req.method === 'GET' && req.url === '/') {
-    const filePath = path.join(__dirname, 'main.html');
+    const filePath = path.join(__dirname, '/public/html/main.html');
     fs.readFile(filePath, (err, data) => {
       if (err) {
+        console.error('Error reading main.html:', err);
         res.statusCode = 500;
         res.setHeader('Content-Type', 'text/plain');
         res.end('Internal Server Error');
@@ -17,6 +18,34 @@ const server = http.createServer((req, res) => {
       }
       res.statusCode = 200;
       res.setHeader('Content-Type', 'text/html');
+      res.end(data);
+    });
+  } else if (req.method === 'GET' && req.url === '/css/main.css') {
+    const filePath = path.join(__dirname, '/public/css/main.css');
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        console.error('Error reading styles.css:', err);
+        res.statusCode = 500;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Internal Server Error');
+        return;
+      }
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/css');
+      res.end(data);
+    });
+  } else if (req.method === 'GET' && req.url === '/js/main.js') {
+    const filePath = path.join(__dirname, '/public/js/main.js');
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        console.error('Error reading main.js:', err);
+        res.statusCode = 500;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Internal Server Error');
+        return;
+      }
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'application/javascript');
       res.end(data);
     });
   } else {
