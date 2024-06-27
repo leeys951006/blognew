e162634e8c5eea0056c35e98e51fa2396944761a
이연승 <leeys951006@gmail.com>
2024-06-27 19:51:58 +0900

    app.js 내용 수정

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!\n');
+  if (req.method === 'GET' && req.url === '/') {
+    const filePath = path.join(__dirname, 'main.html');
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        res.statusCode = 500;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Internal Server Error');
+        return;
+      }
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/html');
+      res.end(data);
+    });
+  } else {
+    res.statusCode = 404;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Not Found');
+  }
 });
 
 server.listen(port, hostname, () => {
